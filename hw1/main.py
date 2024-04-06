859b2706b322c13402fb7cab21bd4c0cfdd9f6e8
杨京 <211250115@smail.nju.edu.cn>
2024-04-06 22:50:30 +0800

    完成第一部分的任务

--- hw1/main.py
+++ hw1/main.py
@@ -1,13 +1,32 @@
 import matplotlib.pyplot as plt  # plt 用于显示图片
 import matplotlib.image as mpimg  # mpimg 用于读取图片
+import numpy as np
 
 img1 = mpimg.imread('I0.jpg')
-plt.imshow(img1)
+# plt.imshow(img1)
 
-print(img1)
+# print(img1)
+
+
+print(img1.size)
+print(img1[0].size)
+
+height = img1.shape[0]
+width = img1.shape[1]
+
+# width, height = img1.size
+
+image1 = np.array(img1)
+
+image = np.zeros((height, width))
+
+for i in range(height):
+    for j in range(width):
+        image[i][j] = image1[i][j][0] * 0.3 + image1[i][j][1] * 0.59 + image1[i][j][2] * 0.11
+
+image = image.astype(np.uint8)
+image2 = plt.imshow(image,cmap='gray')
 
 plt.title('图像1')
-# 不显示坐标轴
 plt.axis('off')
-
 plt.show()
